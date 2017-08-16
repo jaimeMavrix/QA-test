e882182bb22f2ed8b77e9e5cba155bb96b898a21
bibofifa <31038679+bibofifa@users.noreply.github.com>
2017-08-16 12:28:38 +0200

    Update example_spec.js

--- example_spec.js
+++ example_spec.js
@@ -1,11 +1,10 @@
 var KabooPageObject = require ('../test/KabooPageObject.js');
 
 describe('kaboohomepage', function() {
-	browser.get('http://qatest.staging.kaboo.com/');
-	var kaboo = new  KabooPageObject();
-		
-		it('should type user', function() {
-		kaboo.setUsername('kaboo');
-  });
+	it ('should log in', function() {
+	browser.driver.get('https://kaboo:flappybird@qatest.staging.kaboo.com/');
+   
+	it ('should log in', function() {
+ 
   });
 });
