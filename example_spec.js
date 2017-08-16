fa8dae0a704979cf738370d996a36ff3d8da3a72
bibofifa <31038679+bibofifa@users.noreply.github.com>
2017-08-16 09:18:56 +0200

    Update example_spec.js

--- example_spec.js
+++ example_spec.js
@@ -3,11 +3,9 @@ var KabooPageObject = require ('../test/KabooPageObject.js');
 describe('kaboohomepage', function() {
 	browser.get('http://qatest.staging.kaboo.com/');
 	var kaboo = new  KabooPageObject();
-	
-	describe('kaboohomepage', function() {
 		
 		it('should type user', function() {
-		kaboo.loginButton.click();
+		kaboo.setUsername('kaboo');
   });
   });
 });
