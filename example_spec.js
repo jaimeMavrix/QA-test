91a50664bc1cc250f07997da2b187771ce52ac09
bibofifa <31038679+bibofifa@users.noreply.github.com>
2017-08-17 20:19:13 +0300

    Update example_spec.js

--- example_spec.js
+++ example_spec.js
@@ -1,35 +1,38 @@
 var KabooPageObject = require ('./kabooPageObject.js');
 
 describe('kaboohomepage', function() {
-		
-	it ('should log in', function() {
 
-	var baseUrl = ('https://kaboo:flappybird@qatest.staging.kaboo.com/en/login');
-        var url = ("https://qatest.staging.kaboo.com/en/login");
+    it ('should log in', function() {
 
-        browser.driver.get(baseUrl);
-       	browser.driver.get(url);
-       	browser.sleep(5000);
-		});
-		
-	beforeEach(function () {
-	kabooPageObject = new KabooPageObject();
-	});
-	
+        var baseLoginUrl = ('https://kaboo:flappybird@qatest.staging.kaboo.com/en/login');
+        var loginUrl = ("https://qatest.staging.kaboo.com/en/login");
 
-	it ('type username', function() {
+        browser.driver.get(baseLoginUrl);
+        browser.driver.get(loginUrl);
+        browser.sleep(5000);
+
+    });
+
+    beforeEach(function () {
+        kabooPageObject = new KabooPageObject();
+    });
+
+    it ('type username', function() {
         kabooPageObject.setUsername('user')
-	    browser.sleep(5000);
-		});
+        //browser.sleep(1000);
+    });
 
-  	it ('type password', function() {
+    it ('type password', function() {
         kabooPageObject.setPassword('pass')
-        browser.sleep(5000);
-		});
-		
-	it ('type password', function() {
-        kabooPageObject.login()
-        browser.sleep(5000);
-		});
+    });
+
+    it ('click login', function() {
+        kabooPageObject.login();
+    });
+
+    it ('wait message', function() {
+        kabooPageObject.errorMessageDisplayed();
+        browser.sleep(1000);
+    });
 
 } );
