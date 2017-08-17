e2a03b87290b34f9630a8b404e93f61a159c77be
bibofifa <31038679+bibofifa@users.noreply.github.com>
2017-08-17 10:52:50 +0200

    Update example_spec.js

--- example_spec.js
+++ example_spec.js
@@ -1,28 +1,35 @@
 var KabooPageObject = require ('./kabooPageObject.js');
-//var until = require ('selenium-webdriver').until;
 
 describe('kaboohomepage', function() {
+		
 	it ('should log in', function() {
 
-	    var baseUrl = ('https://kaboo:flappybird@qatest.staging.kaboo.com/en/login');
+	var baseUrl = ('https://kaboo:flappybird@qatest.staging.kaboo.com/en/login');
         var url = ("https://qatest.staging.kaboo.com/en/login");
 
         browser.driver.get(baseUrl);
-        browser.driver.get(url);
-        browser.sleep(5000);
-    });
+       	browser.driver.get(url);
+       	browser.sleep(5000);
+		});
+		
+	beforeEach(function () {
+	kabooPageObject = new KabooPageObject();
+	});
+	
 
 	it ('type username', function() {
-        var kabooPageObject = new KabooPageObject();
         kabooPageObject.setUsername('user')
 	    browser.sleep(5000);
-    });
+		});
 
-    it ('type password', function() {
-        var kabooPageObject = new KabooPageObject();
-        //element(password).click();
+  	it ('type password', function() {
         kabooPageObject.setPassword('pass')
         browser.sleep(5000);
-    });
+		});
+		
+	it ('type password', function() {
+        kabooPageObject.login()
+        browser.sleep(5000);
+		});
 
 } );
