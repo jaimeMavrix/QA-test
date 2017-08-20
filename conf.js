ec80b79d1006f02ebf796398d4124cdb96cbc3f4
bibofifa <31038679+bibofifa@users.noreply.github.com>
2017-08-20 23:12:46 +0300

    Delete conf.js

--- conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-	//The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
- //Here we specify the name of the specs files.
- chromeOnly: true,
-directConnect: true,
-  specs: ['loginPage.js']
-}
